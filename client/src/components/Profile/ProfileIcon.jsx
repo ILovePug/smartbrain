71352d724323cbe4d5c9915c7c1b85b8c4ff9fde
Qian Weng <qweng@almo.com>
2018-12-17 14:45:36 -0500

    added modal with react protal

---
+++ client/src/components/Profile/ProfileIcon.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+export class ProfileIcon extends Component {
+  state = {
+    dropdownOpen: false
+  };
+
+  toggle = () => {
+    this.setState(prevState => ({ dropdownOpen: !prevState.dropdownOpen }));
+  };
+  render() {
+    return (
+      <div className="pa4 tc">
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag="span"
+            data-toggle="dropdown"
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              alt="avatar"
+              className="br-100 ba h3 w3 dib"
+            />
+          </DropdownToggle>
+          <DropdownMenu right className="b--transparent shadow-5" style={{marginTop:'20px', backgroundColor:'rgba(255,255,255,0.5)'}}>
+            <DropdownItem onClick={this.props.toggleModal} >View Profile</DropdownItem>
+            <DropdownItem onClick={()=>this.props.onRouteChange('signin')} >Sign out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    );
+  }
+}
+
+export default ProfileIcon;
